ecc6849447c5deb2260f9181d089c38d92b70b7e
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-06-04 23:39:25 +0100

    done

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
-export default function getStudentIdsSum(arr) {
-  return arr.reduce((acc, student) => acc + student.id, 0);
+export default function getStudentIdsSum(stud_arr) {
+  return stud_arr.reduce((acc, student) => acc + student.id, 0);
 }
 
--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,5 @@
-export default function updateStudentGradeByCity(arr, city, newGrades) {
-  return arr
+export default function updateStudentGradeByCity(stud_arr, city, newGrades) {
+  return stud_arr
     .filter(student => student.location === city)
     .map(student => {
       let grade = newGrades.find(g => g.studentId === student.id);
